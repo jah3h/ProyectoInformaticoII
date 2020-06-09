6b5dcbed954770aa6818800ad890a9ea3d6a6b13
InfoChincha <63263834+InfoChincha@users.noreply.github.com>
2020-06-08 22:48:49 -0500

    sdsd

--- index.php
+++ index.php
@@ -3,31 +3,37 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
 
     
     <title>Hello, World!</title>
   </head>
   <body>
-    
-     <div class="row">
-    <div class="col s12 m5">
-      <div class="card darken-1">
-        <div class="card-content white-text">
-          <span class="card-title">Card Title</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
-          <a href="#">This is a link</a>
-        </div>
-      </div>
-    </div>
-  </div>
+  
+    <table>
+      <tr>
+        <td> <input type="text" name="">
+
+        </td>
+      </tr>
+      <tr>
+        <td><input type="password" name=""></td>
+
+      </tr> 
+      <tr>
+        <td><a href="#">Olvidaste tu contraseña</a></td>
+      </tr>
+      <tr>
+        <td>
+          <input type="submit" name="LOGIN" value="LOGIN">
+        </td>
+      </tr>
+    </table>
+
+      
+     
+ 
+ 
 
    
   </body>
